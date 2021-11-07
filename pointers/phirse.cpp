31620b33ab68af7589518a6fe57c7b2c49bcdb7b
vivekkumar2002 <vivekchauhan161202@gmail.com>
2021-11-07 17:07:48 +0530

    added space

--- pointers/phirse.cpp
+++ pointers/phirse.cpp
@@ -4,7 +4,8 @@ int main(){
     int a = 34;
     int* ptra;
     ptra =&a;
-    cout<<"the value of a is "<<a<<endl;
+    cout<<"the value of a  is "<<a<<endl;
+    
     cout<<"the value of a is "<<*ptra<<endl;
     cout<<"the address of a is "<<&a<<endl;
     cout<<"the address of a is " <<ptra<<endl;
