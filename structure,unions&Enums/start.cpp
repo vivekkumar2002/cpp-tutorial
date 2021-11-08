a246e7cd4b3fe0c67a5167ee5c41dfc54d4ed2ca
vivekkumar2002 <vivekchauhan161202@gmail.com>
2021-11-08 19:18:50 +0530

    some new syntax added

--- structure,unions&Enums/start.cpp
+++ structure,unions&Enums/start.cpp
@@ -7,14 +7,28 @@ typedef struct employee
     float salary;
 } ep;
 
+union money
+{
+    int rice;
+    char car;
+    float pounds;
+};
+
+
 int main(){
     ep vivek;
-    vivek.eID = 7;
-    vivek.favchar = 'v';
-    vivek.salary = 100000;
-    cout<<"the value is "<<vivek.salary<<endl;
-    cout<<"the value is "<<vivek.favchar<<endl;
-    cout<<"the value is "<<vivek.eID<<endl;
+    union money m1;
+    m1.rice = 34;
+    // m1.car ='c';
+
+
+    // vivek.eID = 7;
+    // vivek.favchar = 'v';
+    // vivek.salary = 100000;
+    // cout<<"the value is "<<vivek.salary<<endl;
+    // cout<<"the value is "<<vivek.favchar<<endl;
+    // cout<<"the value is "<<vivek.eID<<endl;
+    cout<<m1.rice;
     
 
 
