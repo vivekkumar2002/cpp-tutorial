297e7f80851712a62cc6a447f894e3fbdbd90649
vivekkumar2002 <vivekchauhan161202@gmail.com>
2021-11-08 19:36:34 +0530

    initialize

---
+++ inline_function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+inline int product(int a, int b){
+    return a*b;
+}
+
+int main(){
+    int a,b;
+    cout<<"enter the value of a and b"<<endl;
+    cin>>a>>b;
+    cout<<"the product of a and b is "<<product(a,b);
+   
+    return 0;
+    
+}
