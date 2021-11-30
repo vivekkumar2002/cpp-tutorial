d3fe94c1099247ee104aab8700a21aaf352145eb
vivekkumar2002 <vivekchauhan161202@gmail.com>
2021-11-30 08:19:50 +0530

    initial commit

--- STL01.cpp
+++ STL01.cpp
@@ -1,8 +1,29 @@
 //containers
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <vector>
 
-int main(){
-    
+using namespace std;
+void display(vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    vector<int> vec1;
+    int element, size;
+    cout << "enter the size of your vector" << endl;
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "enter an element to add to this vector: " << endl;
+        cin >> element;
+        vec1.push_back(element);
+    }
+    vec1.pop_back();
+    display(vec1);
     return 0;
 }
--- template.cpp
+++ template.cpp
@@ -11,7 +11,7 @@
 //     int dotproduct(vector &v){
 //          int d=0;
 //          for(int i=0;i<size;i++){
-//              d+= this->arr[i] * v.arr[i];  
+//              d+= this->arr[i] * v.arr[i];
 //          }
 //          return d;
 //     }
@@ -30,36 +30,41 @@
 //     cout<<a<<endl;
 //     return 0;
 // }
-#include<iostream>
+#include <iostream>
 using namespace std;
 template <class T>
-class vector{
-    public:
-    T * arr;
+class vector
+{
+public:
+    T *arr;
     int size;
-    vector(int m){
-        size =m;
+    vector(int m)
+    {
+        size = m;
         arr = new T[size];
     }
-    T dotproduct(vector &v){
-         T d=0;
-         for(int i=0;i<size;i++){
-             d+= this->arr[i] * v.arr[i];  
-         }
-         return d;
+    T dotproduct(vector &v)
+    {
+        T d = 0;
+        for (int i = 0; i < size; i++)
+        {
+            d += this->arr[i] * v.arr[i];
+        }
+        return d;
     }
 };
-int main(){
-    vector <float>v1(3);
-    v1.arr[0]=1.4;
-    v1.arr[1]=3.3;
-    v1.arr[2]=0.1;
+int main()
+{
+    vector<float> v1(3);
+    v1.arr[0] = 1.4;
+    v1.arr[1] = 3.3;
+    v1.arr[2] = 0.1;
 
-    vector <float>v2(3);
-    v2.arr[0]=0.1;
-    v2.arr[1]=1.90;
-    v2.arr[2]=4.1;
+    vector<float> v2(3);
+    v2.arr[0] = 0.1;
+    v2.arr[1] = 1.90;
+    v2.arr[2] = 4.1;
     float a = v1.dotproduct(v2);
-    cout<<a<<endl;
+    cout << a << endl;
     return 0;
 }
